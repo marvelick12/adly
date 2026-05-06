a1451be3034a79cdf07e72e2c5ffb143803c31f4
Marvelick12 <rkjagubng562@gmail.com>
2026-05-06 12:37:19 +0700

    hallo

---
+++ adly.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'vendor/autoload.php';
+use Adly\Adly;
+$adly = new Adly('your_api_key', 'your_api_secret');
+try {
+    $response = $adly->sendMessage('recipient_id', 'Hello, this is a test message!');
+    print_r($response);
+} catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+}
+?>
